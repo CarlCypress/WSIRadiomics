cb48bd1df421474e7ed452c0f7fee42770f90ac6
CarlCypress <CarlCypress@yeah.net>
2025-12-19 09:36:41 +0800

    feat: Repository structure

--- src/model.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2025/1/12 21:15
-# @Author  : D.N. Huang
-# @Email   : CarlCypress@yeah.net
-# @FileName: model.py
-# @Project : my_template
-print('hello world!')
---
+++ src/wsiradiomics/extractor.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2025/12/19 09:00
+# @Author  : D.N. Huang
+# @Email   : CarlCypress@yeah.net
+# @FileName: extractor.py
+# @Project : WSIRadiomics
